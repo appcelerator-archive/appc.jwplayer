155e2a5771ed436c914ec349b96af8013dba5a66
rblalock <rickblalock@mac.com>
2015-03-05 09:16:20 -0500

    Minor tweaks

--- app.js
+++ app.js
@@ -15,27 +15,32 @@ server.on('started', function(){
 });
 
 // a simple user model
-var channel = Arrow.createModel('channel', {
+var model = Arrow.createModel('testmodel', {
 	fields: {
 		title: { type: String }
 	},
 	connector: 'appc.jwplayer',
 	metadata: {
-		endpoint: 'channels'
+		endpoint: 'channels/videos',
+		key: 'videos' // only for nested API calls to JWplayer
 	}
 });
 
 // add the model since we're creating outside of models directory
-server.addModel(channel);
+server.addModel(model);
 
-// { channel_key: 'R3yEYct7' }
-channel.findAll(function(err, coll) {
+// {"channel_key": "8BdtZjpN"}
+// model.findAll(function(err, coll) {
+// 	console.log(arguments);
+// });
+
+model.query({ channel_key: 'R3yEYct7' }, function(err, coll) {
 	console.log(arguments);
 });
 
-channel.findOne('R3yEYct7', function(err, coll) {
-	console.log(arguments);
-});
+// model.findOne('R3yEYct7', function(err, coll) {
+// 	console.log(arguments);
+// });
 
 // start the server
 server.start();
